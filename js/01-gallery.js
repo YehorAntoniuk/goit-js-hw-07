b7f0acad588d6ad891ed632c06d6f51979b4b54b
YehorAntoniuk <antonukdeys@gmail.com>
2022-09-05 20:38:05 +0300

    дз 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) => `
+<div class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener("click", zoomImg);
+
+function zoomImg(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) return;
+
+  let orgUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src=${orgUrl} width="800" height="600">
+`);
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    (item) => `
+<a class="gallery__item" href=${item.original}>
+  <img class="gallery__image" src=${item.preview} alt=${item.description} />
+</a>`
+  )
+  .join("");
+
+gallery.innerHTML = markup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
